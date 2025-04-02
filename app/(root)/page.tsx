482688c3d82414694e6d58a6f90692301c758083
developermartins <martssilvapro@gmail.com>
2025-04-02 15:43:07 -0300

    All startups page & card initial theming done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,24 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({ searchParams }: {
   searchParams: Promise<{ query?: string }>
 }) {
 
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1 },
+      _id: 1,
+      description: "Creative description",
+      image: "https://unsplash.com/pt-br/fotografias/foto-wall-e-OI1ToozsKBw",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
 
   return (
     <>
@@ -18,6 +32,21 @@ export default async function Home({ searchParams }: {
         </p>
 
         <SearchForm query={ query } />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          { query ? `Search results for "${ query }"` : 'All Startups' }
+        </p>
+
+        <ul className="mt-7 card_grid">
+           { posts?.length > 0 ? (posts.map((post) => (
+            <StartupCard key={ post?._id } post={ post } />
+          ))
+        ) : (
+          <p className="no-results">No starttups found</p>
+        ) }
+        </ul>
       </section>
     </>
   );
