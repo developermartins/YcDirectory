24a3675ecb6df0e9c3ec706c134197794b3720ad
developermartins <martssilvapro@gmail.com>
2025-04-03 17:37:44 -0300

    fetch post details done

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,3 +1,6 @@
+import { client } from '@/sanity/lib/client';
+import { STARTUP_BY_ID_QUERY } from '@/sanity/lib/query';
+import { notFound } from 'next/navigation';
 import React from 'react';
 
 export const experimental_ppr = true;
@@ -6,9 +9,13 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
 
   const id = (await params.id);
 
+  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+
+  if (!post) return notFound();
+
   return (
     <>
-      <h1>Startup number { id }</h1>
+      <h1 className='text-3xl'>{ post.title }</h1>
     </>
   );
 };
