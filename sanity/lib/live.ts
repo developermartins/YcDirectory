333008418700c9d8132f74c5ffad473e15d1fd1c
developermartins <martssilvapro@gmail.com>
2025-04-03 16:49:26 -0300

    SanityLive & dynamic cache implemented

--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -4,6 +4,8 @@
 import { defineLive } from "next-sanity";
 import { client } from './client'
 
+import "server-only";
+
 export const { sanityFetch, SanityLive } = defineLive({ 
   client: client.withConfig({ 
     // Live content is currently only available on the experimental API
