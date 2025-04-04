a36bd9bfa230e07e26af498cbd166b388859f208
developermartins <martssilvapro@gmail.com>
2025-04-04 16:56:58 -0300

    setup Sanity Write Client

--- components/View.tsx
+++ components/View.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Ping from './Ping';
 import { client } from '@/sanity/lib/client';
 import { STARTUP_VIEWS_QUERY } from '@/sanity/lib/query';
+import { writeClient } from '@/sanity/lib/write-client';
+import { unstable_after as after } from 'next/server';
 
 const View = async ({ id }: { id: string }) => {
 
@@ -9,9 +11,14 @@ const View = async ({ id }: { id: string }) => {
     .withConfig({ useCdn: false })
     .fetch(STARTUP_VIEWS_QUERY, { id })
 
+    after(async () => await writeClient
+      .patch(id)
+      .set({ views: totalViews + 1 })
+      .commit());
+
   return (
     <div className='view-container'>
-      <div className='absolute -top-2'>
+      <div className='absolute -top-2 -right-2'>
         <Ping />
       </div>
 
