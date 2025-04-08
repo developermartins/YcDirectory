0ce6829786364ad1d92462ddbe3a1be701d842f2
developermartins <martssilvapro@gmail.com>
2025-04-08 15:20:17 -0300

    create heading

--- app/(root)/startup/create/page.tsx
+++ app/(root)/startup/create/page.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 const page = () => {
   return (
-    <div>page</div>
+    <>
+      <section className='pink_container !min-h-[230px]'>
+        <h1 className='heading'>Submit Your Startup</h1>
+      </section>
+    </>
   );
 };
 
