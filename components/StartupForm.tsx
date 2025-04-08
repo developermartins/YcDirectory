cb54f23ea87d05ef930db94c6ac3bae8b5ffe9eb
developermartins <martssilvapro@gmail.com>
2025-04-08 15:41:18 -0300

    form ui implemented

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -1,8 +1,75 @@
-import React from 'react';
+"use client"
+
+import React, { useState } from 'react';
+import { Input } from './ui/input';
+import { Textarea } from './ui/textarea';
 
 const StartupForm = () => {
+
+  const [errors, setErrors] = useState<Record<string, string>>({});
+
   return (
-    <div>StartupForm</div>
+    <form
+      action={() => {}}
+      className='startup-form'
+    >
+      <div>
+        <label htmlFor="title" className='startup-form_label'>Title</label>
+        <Input
+          id='title'
+          name='title'
+          className='startup-form_input'
+          required
+          placeholder='Startup Title'
+        />
+
+        {
+          errors.title && <p className='startup-form_error'>{errors.title}</p>
+        }
+      </div>
+      <div>
+        <label htmlFor="title" className='startup-form_label'>Description</label>
+        <Textarea
+          id='description'
+          name='Description'
+          className='startup-form_textarea'
+          required
+          placeholder='Startup description'
+        />
+
+        {
+          errors.description && <p className='startup-form_error'>{errors.description}</p>
+        }
+      </div>
+      <div>
+        <label htmlFor="category" className='startup-form_label'>Category</label>
+        <Input
+          id='category'
+          name='category'
+          className='startup-form_input'
+          required
+          placeholder='Startup category (Tech, Health, Education, etc.)'
+        />
+
+        {
+          errors.category && <p className='startup-form_error'>{errors.category}</p>
+        }
+      </div>
+      <div>
+        <label htmlFor="link" className='startup-form_label'>Image URL</label>
+        <Input
+          id='link'
+          name='link'
+          className='startup-form_input'
+          required
+          placeholder='Startup Image URL'
+        />
+
+        {
+          errors.link && <p className='startup-form_error'>{errors.link}</p>
+        }
+      </div>
+    </form>
   );
 };
 
